b7897862a065821e505471fbc9283937c6965022
macx python <macxpython@gmail.com>
2025-09-24 05:47:06 +0200

    CISCO - Salted Password & ELF64 - Chiffrement avec le PID

---
+++ challenges/rootme/cryptanalysis/cisco_salted_password/cryptanalysis.py
@@ -0,0 +1,9 @@
+# ====================================================
+# Root-me "CISCO - Salted Password" challenge cryptanalysis
+# https://www.root-me.org/fr/Challenges/Cryptanalyse/CISCO-Salted-Password
+# ====================================================
+
+# Tips:
+# - John the Ripper
+# - https://kmanu225.gitbook.io/cs/cybersecurity/ctf/tools/cracking/john
+# - https://www.cyberciti.biz/faq/understanding-etcshadow-file/
---
+++ challenges/rootme/cryptanalysis/elf64/cryptanalysis.py
@@ -0,0 +1,8 @@
+# ====================================================
+# Root-me "ELF64 - Chiffrement avec le PID" challenge cryptanalysis
+# https://www.root-me.org/fr/Challenges/Cryptanalyse/ELF64-Chiffrement-avec-le-PID
+# ====================================================
+
+
+# Tips:
+# - execve
