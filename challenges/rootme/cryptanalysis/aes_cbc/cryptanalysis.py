72749b05c19ca2bf707aa83a98cdc7f763e0ae53
macxpython <macxpython@gmail.com>
2025-10-01 18:53:06 +0200

    aes ecb bit flipping

---
+++ challenges/rootme/cryptanalysis/aes_cbc/cryptanalysis.py
@@ -0,0 +1,8 @@
+# ====================================================
+# Root-me "AES - CBC - Bit-Flipping Attack" challenge cryptanalysis
+# https://www.root-me.org/fr/Challenges/Cryptanalyse/AES-CBC-Bit-Flipping-Attack?lang=fr
+# ====================================================
+
+# Tips:
+# - p_n = AES(c_n) XOR c_(n-1)
+# - How to get any p_n you want?
