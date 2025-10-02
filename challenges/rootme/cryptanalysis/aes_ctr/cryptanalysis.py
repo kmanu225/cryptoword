8d8934193894a42f9703d3dc43f8af7307781495
macxpython <macxpython@gmail.com>
2025-10-02 20:04:15 +0200

    aes ctr

---
+++ challenges/rootme/cryptanalysis/aes_ctr/cryptanalysis.py
@@ -0,0 +1,9 @@
+# ====================================================
+# Root-me "AES128 - CTR" challenge cryptanalysis
+# https://www.root-me.org/fr/Challenges/Cryptanalyse/AES128-CTR?lang=fr
+# ====================================================
+
+# Tips:
+# - python2.7
+# - Nonce ?
+# - language frequency analysis
